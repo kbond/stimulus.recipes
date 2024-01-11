e2461023295c91452c603afd2e8d8ad2dff08851
Kevin Bond <kevinbond@gmail.com>
2024-01-11 14:27:57 -0500

    update templates

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -7,14 +7,11 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Contracts\Cache\CacheInterface;
 
-#[Route('/', name: 'homepage')]
+#[Route('/', name: 'homepage', methods: ['GET'])]
 class HomepageController extends AbstractController
 {
     public function __invoke(CacheInterface $cache): Response
     {
-        return $this->render('homepage.html.twig', [
-            'controller_name' => 'HomepageController',
-            'random' => $cache->get('random', fn () => random_int(0, 100)),
-        ]);
+        return $this->render('homepage.html.twig');
     }
 }
