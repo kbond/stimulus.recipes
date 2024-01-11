8d25e539e6d21c8ae5c5d75139b9168f997a668a
Kevin Bond <kevinbond@gmail.com>
2024-01-11 14:32:05 -0500

    add getting started page

---
+++ src/Controller/GettingStartedController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/getting-started', name: 'getting_started')]
+class GettingStartedController extends AbstractController
+{
+    public function __invoke(): Response
+    {
+        return $this->render('getting_started.html.twig');
+    }
+}
