6492a2f7178d6e95b3d6e31e417705feb3b58978
Kevin Bond <kevinbond@gmail.com>
2024-01-11 13:48:25 -0500

    disable app cache in dev/test environments

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -5,14 +5,16 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Contracts\Cache\CacheInterface;
 
 #[Route('/', name: 'homepage')]
 class HomepageController extends AbstractController
 {
-    public function __invoke(): Response
+    public function __invoke(CacheInterface $cache): Response
     {
         return $this->render('homepage.html.twig', [
             'controller_name' => 'HomepageController',
+            'random' => $cache->get('random', fn () => random_int(0, 100)),
         ]);
     }
 }
