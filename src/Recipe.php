b6e573b17b0dffb1419799a6aa38a24fd6135dd0
Kevin Bond <kevinbond@gmail.com>
2024-01-12 14:50:52 -0500

    add controller to recipe manifest

--- src/Recipe.php
+++ src/Recipe.php
@@ -6,10 +6,11 @@ namespace App;
  * @author Kevin Bond <kevinbond@gmail.com>
  *
  * @phpstan-type Manifest array{
- *      title: string,
- *      description: string,
- *      demo?: string,
- *      credit?: string|string[],
+ *     title: string,
+ *     description: string,
+ *     controller?: string,
+ *     demo?: string,
+ *     credit?: string|string[],
  *  }
  */
 final class Recipe
@@ -21,6 +22,8 @@ final class Recipe
     /** @var string[] */
     public readonly array $credit;
 
+    private readonly ?string $controller;
+
     /**
      * @param Manifest $manifest
      */
@@ -28,7 +31,18 @@ final class Recipe
     {
         $this->title = $manifest['title'] ?? throw new \InvalidArgumentException('Missing title');
         $this->description = $manifest['description'] ?? throw new \InvalidArgumentException('Missing description');
+        $this->controller = $manifest['controller'] ?? null;
         $this->demo = $manifest['demo'] ?? null;
         $this->credit = (array) ($manifest['credit'] ?? []);
+    }
+
+    public function controllerName(): ?string
+    {
+        return $this->controller ? basename($this->controller) : null;
+    }
+
+    public function controllerSource(): ?string
+    {
+        return $this->controller ? file_get_contents($this->controller) : null;
     }
 }
