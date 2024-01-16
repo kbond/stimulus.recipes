bb7a00210c806f38b40ef70cbf5e34210c999632
Kevin Bond <kevinbond@gmail.com>
2024-01-16 16:57:45 -0500

    improve tabs

--- assets/controllers/tabs_controller.js
+++ assets/controllers/tabs_controller.js
@@ -1,36 +1,23 @@
 import {Controller} from "@hotwired/stimulus"
 
 export default class extends Controller {
-    static classes = ['active']
-    static targets = ["btn", "tab"]
-    static values = {defaultTab: String}
-
-    connect() {
-        // first, hide all tabs
-        this.tabTargets.map(x => x.hidden = true)
-
-        // then, show the default tab
-        let selectedTab = this.tabTargets.find(element => element.id === this.defaultTabValue)
-        selectedTab.hidden = false
-
-        // and activate the selected button
-        let selectedBtn = this.btnTargets.find(element => element.dataset.target === this.defaultTabValue)
-        selectedBtn.classList.add(...this.activeClasses)
-    }
+    static targets = ["tab", "panel"]
 
     // switch between tabs
     // add to your buttons: data-action="click->tabs#select"
     select(event) {
-        // find tab matching (with same id as) the clicked btn
-        let selectedTab = this.tabTargets.find(element => element.id === event.currentTarget.dataset.target)
-        if (selectedTab.hidden) {
-            // hide everything
-            this.tabTargets.map(x => x.hidden = true) // hide all tabs
-            this.btnTargets.map(x => x.classList.remove(...this.activeClasses)) // deactive all btns
+        let currentTab = event.currentTarget;
 
-            // then show selected
-            selectedTab.hidden = false // show current tab
-            event.currentTarget.classList.add(...this.activeClasses) // activate current button
-        }
+        // remove aria-selected from all tabs
+        this.tabTargets.map(tab => tab.ariaSelected = false);
+
+        // add aria-selected to current tab
+        currentTab.ariaSelected = true;
+
+        // hide all panels
+        this.panelTargets.map(panel => panel.hidden = true);
+
+        // find matching panel and unhide
+        this.panelTargets.find(panel => panel.id === currentTab.getAttribute('aria-controls')).hidden = false;
     }
 }
