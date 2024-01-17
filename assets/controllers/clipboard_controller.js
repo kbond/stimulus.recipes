9a0df2046228b609cf1ede57b95060edbdd1be1d
Kevin Bond <kevinbond@gmail.com>
2024-01-17 16:05:37 -0500

    improve clipboard recipe

--- assets/controllers/clipboard_controller.js
+++ assets/controllers/clipboard_controller.js
@@ -2,10 +2,14 @@ import { Controller } from '@hotwired/stimulus';
 
 export default class extends Controller {
     static targets = ['source'];
+    static values = { source: String };
 
     copy(event) {
         event.preventDefault();
-        navigator.clipboard.writeText(this.sourceTarget.innerHTML || this.sourceTarget.value);
+
+        navigator.clipboard.writeText(
+            this.hasSourceValue ? this.sourceValue : this.sourceTarget.innerHTML || this.sourceTarget.value
+        );
         // todo we should dispatch an event on success (ie tooltip event listener can listen to it)
     }
 }
