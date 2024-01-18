2505bf3a9b2d7f223c54183591b29a51a563f37c
Kevin Bond <kevinbond@gmail.com>
2024-01-18 12:42:17 -0500

    improve highlight-code recipe to allow using content block

--- src/Twig/Components/HighlightCode.php
+++ src/Twig/Components/HighlightCode.php
@@ -9,7 +9,7 @@ use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
 #[AsTwigComponent]
 final class HighlightCode
 {
-    public string $code;
+    public ?string $code = null;
     public string $language;
     public ?string $theme = null;
 
@@ -29,13 +29,19 @@ final class HighlightCode
     {
     }
 
-    public function highlighted(): string
+    public function highlight(string $code = null): string
     {
+        $code ??= $this->code;
+
+        if (null === $code) {
+            throw new \RuntimeException('Code must be passed as the content block or the code attribute.');
+        }
+
         // we cache the highlighted code to avoid calling shiki for the same code block on every request
         return $this->cache->get(
-            sprintf('highlight-code-%s_%s_%s', crc32($this->code), $this->language, $this->theme),
+            sprintf('highlight-code-%s_%s_%s', crc32($code), $this->language, $this->theme),
             fn () => Shiki::highlight(
-                code: $this->code,
+                code: $code,
                 language: $this->language,
                 theme: $this->theme,
                 highlightLines: $this->highlightLines,
