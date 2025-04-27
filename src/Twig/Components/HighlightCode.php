8c72209f030ba889b03f2f32b983f3d5c5044a20
Kevin Bond <kevinbond@gmail.com>
2025-04-27 10:25:13 -0400

    use tempest/highlight instead of shiki

--- src/Twig/Components/HighlightCode.php
+++ src/Twig/Components/HighlightCode.php
@@ -2,53 +2,24 @@
 
 namespace App\Twig\Components;
 
-use Spatie\ShikiPhp\Shiki;
-use Symfony\Contracts\Cache\CacheInterface;
 use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
+use Tempest\Highlight\Highlighter;
 
 #[AsTwigComponent]
 final class HighlightCode
 {
     public ?string $code = null;
     public string $language;
-    public ?string $theme = null;
 
-    /** @var list<int>|null */
-    public ?array $highlightLines = null;
-
-    /** @var list<int>|null */
-    public ?array $addLines = null;
-
-    /** @var list<int>|null */
-    public ?array $deleteLines = null;
-
-    /** @var list<int>|null */
-    public ?array $focusLines = null;
-
-    public function __construct(private CacheInterface $cache)
+    public function __construct(private Highlighter $highlighter = new Highlighter())
     {
     }
 
     public function highlight(?string $code = null): string
     {
-        $code ??= $this->code;
-
-        if (null === $code) {
-            throw new \RuntimeException('Code must be passed as the content block or the code attribute.');
-        }
-
-        // we cache the highlighted code to avoid calling shiki for the same code block on every request
-        return $this->cache->get(
-            sprintf('highlight-code-%s_%s_%s', crc32($code), $this->language, $this->theme),
-            fn () => Shiki::highlight(
-                code: $code,
-                language: $this->language,
-                theme: $this->theme,
-                highlightLines: $this->highlightLines,
-                addLines: $this->addLines,
-                deleteLines: $this->deleteLines,
-                focusLines: $this->focusLines,
-            ),
+        return $this->highlighter->parse(
+            $code ?? $this->code ?? throw new \RuntimeException('Code must be passed as the content block or the code attribute.'),
+            $this->language ?? throw new \RuntimeException('Language must be passed as the language attribute.'),
         );
     }
 }
